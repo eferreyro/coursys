43e66592b89fa9f105a13454ac0c6f0685c4706b
eferreyro <emafe7472@gmail.com>
2021-03-13 21:02:37 -0600

    Creando modelos y migraciones para Reviews y Profiles

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -5,11 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Course extends Model
+class Profile extends Model
 {
     use HasFactory;
-    
-    const BORRADOR = 1;
-    const REVISION = 2;
-    const PUBLICADO = 3;
 }
